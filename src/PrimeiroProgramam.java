4c1c984cfc124ea82c2dd3954ae249b3e000efba
andertrunks <anderluiscosta@gmail.com>
2021-11-29 13:48:57 -0300

    frist commit

---
+++ src/PrimeiroProgramam.java
@@ -0,0 +1,16 @@
+import br.com.dio.model.Gato;
+
+public class PrimeiroProgramam {
+
+	public static void main(String[] args) {
+
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		// TODO Auto-generated method stub
+		/*int a = 5;
+		int b = 2;
+		System.out.println("Hello World! " + (a+b));*/
+	}
+
+}
